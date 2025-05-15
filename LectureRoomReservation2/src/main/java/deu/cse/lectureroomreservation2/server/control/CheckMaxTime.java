d89c00ea755e0748b719923a72f491cd0e6db41c
dsh38 <devsedah@gmail.com>
2025-05-15 19:48:45 +0900

    update

---
+++ LectureRoomReservation2/src/main/java/deu/cse/lectureroomreservation2/server/control/CheckMaxTime.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package deu.cse.lectureroomreservation2.server.control;
+
+/**
+ *
+ * @author H
+ */
+public class CheckMaxTime {
+    private String id;
+    
+    public CheckMaxTime(String id) {
+        this.id = id;
+    }
+    
+    public boolean check() {
+        boolean result = false;
+        
+        return result;
+    }
+}
