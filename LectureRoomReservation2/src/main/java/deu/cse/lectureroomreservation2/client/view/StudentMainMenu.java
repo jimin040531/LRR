fbbddf42a3ceed903203b3ca6b044f6994e99b7c
dsh38 <devsedah@gmail.com>
2025-05-24 23:41:20 +0900

    restore StudentMainMenu.java

--- LectureRoomReservation2/src/main/java/deu/cse/lectureroomreservation2/client/view/StudentMainMenu.java
+++ LectureRoomReservation2/src/main/java/deu/cse/lectureroomreservation2/client/view/StudentMainMenu.java
@@ -212,6 +212,9 @@ public class StudentMainMenu extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(StudentMainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
